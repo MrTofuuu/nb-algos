5b97a09ecb67b989e27118013de869c4816b4b73
MrTofuuu <chris.stallcup@gmail.com>
2022-03-25 16:37:54 -0500

    removing solutions

--- 03-log-nums/Unsolved/log-nums.js
+++ 03-log-nums/Unsolved/log-nums.js
@@ -2,13 +2,5 @@
 // Assume `num` will be a positive number
 
 var logNums = function(num) {
-    // start count at 1
-    var output = 1;
-    // for loop to iterate 1 through num 
-    for (i = 0; i < num; i++) {
-        // prints output 
-        console.log(output);
-        // output goes up by 1 after printing 
-        output++;
-    }
+    
 };
